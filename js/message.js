7bfa9d43623a8435287c60c2015841586479fd96
monika001 <czarkowska.monika@gmail.com>
2017-01-22 21:11:40 +0100

    added all files

---
+++ js/message.js
@@ -0,0 +1,8 @@
+﻿var message = "Zapraszam do zapoznania się  moją stroną!";
+
+function updateMessage(){
+	var el = document.getElementById('message');
+	el.textContent = message;
+}
+
+updateMessage();
